af0be9101a5ab72e1cefb2d5aa332cfd92acfca1
anniqueclaringbould <anniqueclaringbould@gmail.com>
2026-04-22 13:40:19 +0000

    Deploying to gh-pages from @ anniqueclaringbould/anniqueclaringbould.github.io@a6f484e7bfeda59e8d276ab45872ad2792677061 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -49,11 +49,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-annique-team-started-her-work-at-erasmus-mc",
           title: '[Annique](/team/) started her work at Erasmus MC',
           description: "",
